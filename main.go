5cd521b4baf02401acfebd406e3bcc4362c5a409
sidiqw <sidiq.widyarso@gmail.com>
2024-04-28 11:50:47 +0700

    Changes to be committed: 	modified:   dockerfile 	new file:   go.mod 	new file:   main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Nama Environment bisa diambil dari environment variable
+	namaEnvironment := os.Getenv("Environment_Name")
+
+	fmt.Fprintf(w, "<h1>Nama Aplikasi: Seeds Test</h1>")
+	fmt.Fprintf(w, "<p>Nama Environment: %s</p>", namaEnvironment)
+	fmt.Fprintf(w, "<p>Version: 1.0</p>")
+	fmt.Fprintf(w, "<p>Status: OK</p>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+}
