5ffdb1683bcaeecfc63a491751ac7d9e3cfbe7d3
Hamda <88338758+hamdahamda@users.noreply.github.com>
2024-05-02 03:35:49 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Nama Environment bisa diambil dari environment variable
-	os.Setenv("Environment_Name", "Development")
+	os.Setenv("Environment_Name", "Production")
 	
 	environmentName := os.Getenv("Environment_Name")
 
